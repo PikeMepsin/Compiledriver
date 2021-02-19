783e32cb536124b848258be72d12d10f794f7848
PikeMepsin <michael.pepsin1@marist.edu>
2021-02-18 21:26:10 -0500

    helloWorld program added, updated readme with compilation instructions

---
+++ lab0/helloWorld.java
@@ -0,0 +1,7 @@
+public class helloWorld {
+
+  // My name is Mike Pepsin and I will be using Java
+  public static void main(String[] args) {
+    System.out.println("Hello World! I am ready to run the gauntlet that is Compilers!");
+  }
+}
