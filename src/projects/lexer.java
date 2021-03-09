4d9ee0f10bff8a5a0c60fbffce506f97f3037f3e
PikeMepsin <michael.pepsin1@marist.edu>
2021-03-09 16:57:34 -0500

    reworked data structures, added unrecognized character error, main lex loop got some upgrades

--- src/projects/lexer.java
+++ src/projects/lexer.java
@@ -5,70 +5,72 @@
 
 import java.util.Scanner;
 import java.util.HashMap;
+import java.util.ArrayList;
 
 public class lexer {
-  
-  //hashmap for determining which column to use in the dfa matrix during lex
-  HashMap<Integer, String> dfaColumns = new HashMap<>();
-  dfaColumns.put(0, "a");
-  dfaColumns.put(1, "b");
-  dfaColumns.put(2, "c");
-  dfaColumns.put(3, "d");
-  dfaColumns.put(4, "e");
-  dfaColumns.put(5, "f");
-  dfaColumns.put(6, "g");
-  dfaColumns.put(7, "h");
-  dfaColumns.put(8, "i");
-  dfaColumns.put(9, "j");
-  dfaColumns.put(10, "k");
-  dfaColumns.put(11, "l");
-  dfaColumns.put(12, "m");
-  dfaColumns.put(13, "n");
-  dfaColumns.put(14, "o");
-  dfaColumns.put(15, "p");
-  dfaColumns.put(16, "q");
-  dfaColumns.put(17, "r");
-  dfaColumns.put(18, "s");
-  dfaColumns.put(19, "t");
-  dfaColumns.put(20, "u");
-  dfaColumns.put(21, "v");
-  dfaColumns.put(22, "w");
-  dfaColumns.put(23, "x");
-  dfaColumns.put(24, "y");
-  dfaColumns.put(25, "z");
-  dfaColumns.put(26, "0");
-  dfaColumns.put(27, "1");
-  dfaColumns.put(28, "2");
-  dfaColumns.put(29, "3");
-  dfaColumns.put(30, "4");
-  dfaColumns.put(31, "5");
-  dfaColumns.put(32, "6");
-  dfaColumns.put(33, "7");
-  dfaColumns.put(34, "8");
-  dfaColumns.put(35, "9");
-  dfaColumns.put(36, "{");
-  dfaColumns.put(37, "}");
-  dfaColumns.put(38, "(");
-  dfaColumns.put(39, ")");
-  dfaColumns.put(40, "!");
-  dfaColumns.put(41, "=");
-  dfaColumns.put(42, "+");
-  dfaColumns.put(43, "\"");
-  dfaColumns.put(44, "/");
-  dfaColumns.put(45, "*");
-  dfaColumns.put(46, "$");
-  dfaColumns.put(47, " ");
-  
   public static void main(String[] args) {
+    
+    //hashmap for determining which column to use in the dfa matrix during lex
+    HashMap<String, Integer> dfaColumns = new HashMap<>();
+    dfaColumns.put("a", 0);
+    dfaColumns.put("b", 1);
+    dfaColumns.put("c", 2);
+    dfaColumns.put("d", 3);
+    dfaColumns.put("e", 4);
+    dfaColumns.put("f", 5);
+    dfaColumns.put("g", 6);
+    dfaColumns.put("h", 7);
+    dfaColumns.put("i", 8);
+    dfaColumns.put("j", 9);
+    dfaColumns.put("k", 10);
+    dfaColumns.put("l", 11);
+    dfaColumns.put("m", 12);
+    dfaColumns.put("n", 13);
+    dfaColumns.put("o", 14);
+    dfaColumns.put("p", 15);
+    dfaColumns.put("q", 16);
+    dfaColumns.put("r", 17);
+    dfaColumns.put("s", 18);
+    dfaColumns.put("t", 19);
+    dfaColumns.put("u", 20);
+    dfaColumns.put("u", 21);
+    dfaColumns.put("w", 22);
+    dfaColumns.put("x", 23);
+    dfaColumns.put("y", 24);
+    dfaColumns.put("z", 25);
+    dfaColumns.put("0", 26);
+    dfaColumns.put("1", 27);
+    dfaColumns.put("2", 28);
+    dfaColumns.put("3", 29);
+    dfaColumns.put("4", 30);
+    dfaColumns.put("5", 31);
+    dfaColumns.put("6", 32);
+    dfaColumns.put("7", 33);
+    dfaColumns.put("8", 34);
+    dfaColumns.put("9", 35);
+    dfaColumns.put("{", 36);
+    dfaColumns.put("}", 37);
+    dfaColumns.put("(", 38);
+    dfaColumns.put(")", 39);
+    dfaColumns.put("!", 40);
+    dfaColumns.put("=", 41);
+    dfaColumns.put("+", 42);
+    dfaColumns.put("\"", 43);
+    dfaColumns.put("/", 44);
+    dfaColumns.put("*", 45);
+    dfaColumns.put("$", 46);
+    dfaColumns.put(" ", 47);
     
     //variables for lexing
     Scanner lex = new Scanner(System.in);
-    String lexeme = "";
+    String codeFragment = "";
+    ArrayList<Token> tokenStream = new ArrayList<>();
     
     //counters
     int progCounter = 1;
     int warnings = 0;
     int errors = 0;
+    int state = 1;
     
     //the following declarations are flags that we'll use later
     boolean inQuotes = false;
@@ -76,21 +78,56 @@ public class lexer {
     int inBrackets = 0;
     int inParens = 0;
         
-    int dfa[][] = {/*a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 { } ( ) ! = + " / * $  */
+    int dfa[][] = {/*a b c d e f g h i j k l m n o p q r s t u v w x y z 0 1 2 3 4 5 6 7 8 9 { } ( ) ! = + " / * $ space*/
     /* state 0 */   {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
     /* state 1 */   {2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    /* state 2 */   {3,0,0,0,0,9,0,5,0,0,0,0,0,3,3,0,0,3,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    /* state 3 */   {0,0,0,0,6,0,0,0,4,0,0,4,0,0,4,0,0,4,0,9,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    /* state 4 */   {0,0,0,0,9,0,0,0,5,0,0,3,0,5,0,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    /* state 5 */   {0,0,0,0,9,0,0,0,9,0,0,0,0,9,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    /* state 6 */   {7,0,0,0,0,0,9,0,0,0,0,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-    /* state 7 */   {0,0,0,0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
+    /* state 2 */   {3,0,0,0,0,9,0,3,0,0,0,0,0,3,3,0,0,3,0,3,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    /* state 3 */   {0,0,0,0,0,0,0,0,4,0,0,4,0,0,5,0,0,4,0,9,4,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    /* state 4 */   {0,0,0,0,9,0,0,0,5,0,0,5,0,5,0,0,0,0,5,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    /* state 5 */   {0,0,0,0,9,0,0,0,0,0,0,6,0,6,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    /* state 6 */   {0,0,0,0,7,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    /* state 7 */   {8,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+    /* state 8 */   {0,0,0,0,0,0,0,0,0,0,0,0,0,9,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0}};
     
+    //position variables
+    int line = 0;
+    int pos = 0;
+    
+    //position variables for lexing
+    int currentPos = 0;
+    int lastPos = currentPos + 1;
+    
+    //main loop for lexing through the code
     while (lex.hasNextLine()) {
       while (lex.hasNext()) {
-        code = lex.next();
-        System.out.println(code);
+        String next = lex.next();
+        pos++;
+        if (!dfaColumns.containsKey(next)) {
+          errors++;
+          for (Token : tokenStream) {
+            printStream(Token.type, Token.lexeme);
+          }
+          System.out.println("Lex terminated with " + errors + "error(s) at [" + line + "," + pos + "]");
+          System.out.println("ERROR: Unrecognized character " + next);
+          // TODO remove break, move on to next program once error is found after 1 is working
+          break;
+        }
+        
+        codeFragment += next;
+        System.out.println(codeFragment);
       }
+      line++;
+    }
+  }
+  
+  public static class Token {
+    //class variables
+    public String type;
+    public String lexeme;
+    
+    public Token(String type, String lexeme) {
+      this.type = type;
+      this.lexeme = lexeme;
     }
   }
   
