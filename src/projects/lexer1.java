a24c5f707e590a036947c700d2820154dbbfeeb9
PikeMepsin <michael.pepsin1@marist.edu>
2021-03-26 04:10:45 -0400

    catching IDs and whitespace, also printing token list + warning with no EOP

--- src/projects/lexer1.java
+++ src/projects/lexer1.java
@@ -17,7 +17,7 @@ public class lexer1 {
     TYPEBOOLEAN("boolean"),
     BOOLVALT("true"),
     BOOLVALF("false"),
-    ID("[a-z]{1}"),
+    ID("[a-z]"),
     EOP("[$]"),
     BOOLEQ("=="),
     BOOLINEQ("!="),
@@ -52,8 +52,10 @@ public class lexer1 {
     boolean inComments = false;
     boolean inBrackets = false;
     boolean inParens = false;
+    boolean printed = false;
     
     while (lex.hasNextLine()) {
+      printed = false;
       String input = lex.nextLine();
       
       StringBuffer patternBuilder = new StringBuffer();
@@ -61,36 +63,76 @@ public class lexer1 {
       for (TokenNames tokenName : TokenNames.values()) {
         patternBuilder.append(String.format("|(?<%s>%s)", tokenName.name(), tokenName.pattern));
       }
-      //patternBuilder.append("|hello");
             
       Pattern lexemes = Pattern.compile(patternBuilder.substring(1), Pattern.DOTALL);
       Matcher snoop = lexemes.matcher(input);
       
-      //System.out.println(lexemes);
-      //System.out.println(patternBuilder);
-      //MatchResult result = snoop.toMatchResult();
-      //System.out.println(result.group());
-      
       while(snoop.find()) {
     	//for testing purposes
-        System.out.println("snoop found");
-        
+        //System.out.println("snoop found");
         //System.out.println(snoop.group());
         
-        if (snoop.group(TokenNames.PRINT.name()) != null) {
+        if (snoop.group(TokenNames.WHITESPACE.name()) != null) {
+          // process whitespace, do nothing with it
+        }
+        else if (snoop.group(TokenNames.PRINT.name()) != null) {
           tokens.add(new Token("PRINT", snoop.group(TokenNames.PRINT.name()),
           line, snoop.start()));
-          System.out.println("print token added");
+          //System.out.println("print token added");
+        }
+        else if (snoop.group(TokenNames.WHILE.name()) != null) {
+          tokens.add(new Token("WHILE", snoop.group(TokenNames.WHILE.name()),
+          line, snoop.start()));
+        }
+        else if (snoop.group(TokenNames.IF.name()) != null) {
+          tokens.add(new Token("IF", snoop.group(TokenNames.IF.name()),
+          line, snoop.start()));
+        }
+        else if (snoop.group(TokenNames.TYPEINT.name()) != null) {
+          tokens.add(new Token("TYPEINT", snoop.group(TokenNames.TYPEINT.name()),
+          line, snoop.start()));
+        }
+        else if (snoop.group(TokenNames.TYPESTRING.name()) != null) {
+          tokens.add(new Token("TYPESTRING", snoop.group(TokenNames.TYPESTRING.name()),
+          line, snoop.start()));
+        }
+        else if (snoop.group(TokenNames.TYPEBOOLEAN.name()) != null) {
+          tokens.add(new Token("TYPEBOOLEAN", snoop.group(TokenNames.TYPEBOOLEAN.name()),
+          line, snoop.start()));
+        }
+        else if (snoop.group(TokenNames.BOOLVALT.name()) != null) {
+          tokens.add(new Token("BOOLVALT", snoop.group(TokenNames.BOOLVALT.name()),
+          line, snoop.start()));
+        }
+        else if (snoop.group(TokenNames.BOOLVALF.name()) != null) {
+          tokens.add(new Token("BOOLVALF", snoop.group(TokenNames.BOOLVALF.name()),
+          line, snoop.start()));
+        }
+        else if (snoop.group(TokenNames.ID.name()) != null) {
+          tokens.add(new Token("ID", snoop.group(TokenNames.ID.name()),
+          line, snoop.start()));
         }
         else if (snoop.group(TokenNames.EOP.name()) != null) {
           tokens.add(new Token("EOP", snoop.group(TokenNames.EOP.name()),
           line, snoop.start()));
-          System.out.println("print token added");
+          //System.out.println("print token added");
           printStream(tokens, progCounter);
+          printed = true;
           progCounter++;
           line = 1;
           tokens.clear();
         }
+      }
+      line++;
+    }
+    if (!printed) {
+      Token lastToken = tokens.get(tokens.size() -1);
+      String lastLexeme = lastToken.lexeme;
+      if (!lastLexeme.equals("$") && !printed) {
+        warnings++;
+        printStream(tokens, progCounter);
+        System.out.println("Finished lexing with " + warnings + " warnings and " + errors + " errors");
+        System.out.println("No end-of-program symbol found");
       }
     }
     
@@ -116,7 +158,8 @@ public class lexer1 {
     System.out.println("INFO - Compilation started");
     System.out.println("INFO - Compiling Program " + progNum);
     for (Token token: tokens) {
-      output = String.format("DEBUG - Lexer - %s [ %s ] found at (%d:%d", token.name, token.lexeme, token.lineNum, token.position);
+      output = String.format("DEBUG - Lexer - %s [ %s ] found at (%d:%d)", token.name, token.lexeme, token.lineNum, token.position);
+      System.out.println(output);
     }
   }
 }
