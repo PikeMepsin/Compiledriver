67ff538c4ef8d9720ab7ac3db510c84e14328447
PikeMepsin <michael.pepsin1@marist.edu>
2021-04-06 16:16:56 -0400

    parse method skeletons written

--- src/projects/parser.java
+++ src/projects/parser.java
@@ -2,8 +2,9 @@ import java.util.ArrayList;
 
 public class parser {
   
+  int index = 0;
   int errors = 0;
-  ArrayList<Token> stream = new ArrayList<Token>();
+  ArrayList<Token> input = new ArrayList<Token>();
   CST tree = new CST();
   
   public parser() {
@@ -11,10 +12,9 @@ public class parser {
   }
   
   public parser(ArrayList<Token> lexOutput) {
-    stream = lexOutput;
-    int index = 0;
+    input = lexOutput;
     
-    if (stream.size() < 3) {
+    if (input.size() < 3) {
       System.out.println("PARSE ERROR: a program must at least consist of {}$");
     }
     parseProgram();
@@ -26,7 +26,7 @@ public class parser {
     
     tree.growBranch("Program");
     parseBlock();
-    isMatch = match("$");
+    isMatch = match("EOP");
     // for formatting
     System.out.println();
     
@@ -35,25 +35,195 @@ public class parser {
   
   public boolean parseBlock() {
     boolean isMatch = false;
-    // TODO
+    if (errors == 0) {
+      match("LBRACE");
+      parseStatementList();
+      isMatch = match("RBRACE");
+    }
     return isMatch;
   }
   
   public boolean parseStatementList() {
     boolean isMatch = false;
-    // TODO
+    if (errors == 0) {
+      
+    }
     return isMatch;
   }
-  public boolean parseEOP() {
+  
+  public boolean parseStatement() {
     boolean isMatch = false;
-    // TODO
+    if (errors == 0) {
+      
+    }
     return isMatch;
   }
+  
+  public boolean parsePrintStatement() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseAssignmentStatement() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseVarDecl() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseWhileStatement() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseIfStatement() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseExpr() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseIntExpr() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseStringExpr() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+
+  public boolean parseBooleanExpr() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseId() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseCharList() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseType() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseChar() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseSpace() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+  
+  public boolean parseDigit() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+
+  public boolean parseBoolOp() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+
+  public boolean parseBoolVal() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+
+  public boolean parseIntOp() {
+    boolean isMatch = false;
+    if (errors == 0) {
+      
+    }
+    return isMatch;
+  }
+
+
+
+
   
   public boolean match(String expectedToken) {
     boolean isMatch = false;
     if (errors == 0) {
-      // TODO
+      if (input.get(index).name.equals(expectedToken)) {
+        tree.sproutLeaf(input.get(index).lexeme);
+        isMatch = true;
+      }
+      
+      if (index < input.size()) {
+        index++;
+      }
+      
+      if (!isMatch) {
+        String errMessage = String.format("ERROR at (%d:%d) : PARSER expected %s, found $s", input.get(index).lineNum, input.get(index).position, expectedToken, input.get(index).lexeme);
+        System.out.println(errMessage);
+        errors++;
+      }
     }
     return isMatch;
   }
